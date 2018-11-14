262e72ba072a1795a0051a4588eb6d396d561016
Westly Meza <westlymeza@gmail.com>
2018-11-13 21:53:37 -0600

    Primer commit

---
+++ src/Components/Navigation.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'React';
+
+class Navigation extends Component
+{
+    render(){
+        return(
+            <nav className = "navbar navbar-dark bg-dar">
+                <a href="#" className="text-white">Taks</a>
+            </nav>
+        )
+    }
+}
+
+export default Navegation;
