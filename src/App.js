6dc227222924b62d8a448fffc6ec61efa4aada00
Westly Meza <westlymeza@gmail.com>
2018-11-16 06:35:45 -0600

    Todo form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './Components/Navigation';
 import TaskCard from './Components/TaskCard';
+import FormTodo from './Components/TodoForm';
 import {todos} from './todos.json';
 console.log(todos);
 class App extends Component {
@@ -18,19 +19,6 @@ class App extends Component {
     const todos = this.state.todos.map((todo,index)=>
     {
       return (
-        /* ITERANDO
-
-
-        <div className="col-md-4" key ={index}>
-          <div className="card mt-4">
-              <div className="card-header">
-                  <h3>{todo.title}</h3>
-              </div>
-              <div className="card-body">
-                  <p>{todo.description}</p>
-              </div>
-          </div>
-      </div>*/
       <TaskCard title = {todo.title} description ={todo.description} key={index} priority={todo.priority}/>
       )
       
@@ -42,6 +30,7 @@ class App extends Component {
           <div className="row mt-4">
           {todos}
           </div>
+          <FormTodo/>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         
@@ -61,4 +50,18 @@ this.state = {
       "title": "Aplicación de Tareas",
       "ntareas": 10
     }
- */
+
+  --Iterando se podian presentar las tareas 
+    de la siguiente forma sin hacer uso de componentes
+
+
+        <div className="col-md-4" key ={index}>
+          <div className="card mt-4">
+              <div className="card-header">
+                  <h3>{todo.title}</h3>
+              </div>
+              <div className="card-body">
+                  <p>{todo.description}</p>
+              </div>
+          </div>
+      </div>*/
--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -4,8 +4,28 @@ class TodoForm extends Component
 {
     render(){
         return(
-            <div></div>
+            <div className="card">
+                <div className="card-body">
+                    <div className="form-group">
+                        <input type="text" name="title"  
+                        onChange = {this.handleInput}
+                        className="form-control" placeholder="Title" />  
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="responsible"  
+                        className="form-control" placeholder="Responsible" />  
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="description"  
+                        className="form-control" placeholder="Description" />  
+                    </div>
+                </div>
+            </div>
         )
+    }
+    handleInput(e)
+    {
+        console.log(e.target.value);
     }
 }
 
